56377784edde2f9501528c44202aede844ceee4b
Ionut F <fetcuionut07@gmail.com>
2025-10-18 12:29:31 +0100

    update

---
+++ input_task.py
@@ -0,0 +1,6 @@
+# Ask user for their name
+
+print("What is your name")
+name = input()
+print(f"It is nice to meet you {name}")
+
---
+++ output_tasks.py
@@ -0,0 +1,16 @@
+#Display message on the screen
+print("Welcome to QHO444!")
+print("In week 1 we will learn...")
+
+print("...How tho use Git and Github")
+print("...How to output to the screen")
+print("...How to get user input")
+print("")
+print("I hope you are enjoying the lesson thus far")
+
+
+
+print ("--------")
+print (" O    O ")
+print ("    -   ")
+print ("---------")
