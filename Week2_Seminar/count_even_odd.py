1cdef97cbcb3007adf391f019553cf5623911463
Ionut F <fetcuionut07@gmail.com>
2025-10-25 13:31:14 +0100

    update - working variant

--- Week2_Seminar/count_even_odd.py
+++ Week2_Seminar/count_even_odd.py
@@ -1,14 +1,14 @@
-print("Enter the first whole number:")
-first_num = int(input())
-print ("Enter the second whole number:")
-second_num = int(input())
-print ("Enter the second whole number:")
-third_num = int(input())
 even_count = 0
 odd_count = 0
-if first_num % 2 == 0:
-    if second_num % 2 == 0:
+
+n1 = int(input('Enter the first whole number: '))
+n2 = int(input('Enter the second whole number: '))
+n3 = int(input('Enter the third whole number: '))
+
+for n in (n1, n2, n3):
+    if n % 2 == 0:
+        even_count += 1
+    else:
         odd_count += 1
-    if third_num % 2 == 0:
-        odd_count += 1
-print (f"There were {even_count} even numbers and {odd_count} odd numbers.")
+print(f"\n There were {even_count} even and {odd_count} odd numbers.")
+
