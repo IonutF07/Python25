f2b4ceb480fe339a5586bcdb9fca1110d682ec09
Ionut F <fetcuionut07@gmail.com>
2025-10-25 12:38:49 +0100

    I need to learn git

---
+++ Week2_Seminar/if_statements.py
@@ -0,0 +1,12 @@
+print ("What kind of book are you reading?")#
+bookname = input()
+if bookname.lower() == "adventure":
+    print (f"I like {bookname} books!.")
+    print ("\n Finished reading book.")
+
+
+print ("Please enter the activity to be performed")#
+activity = input()
+if activity.lower() == "calculate":
+    print ("Performing Calculations...:")
+    print ("Activity completed!")
