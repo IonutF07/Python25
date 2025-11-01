a265faf35306ab9e7268d4c8c304f463a2c7f49c
Ionut F <fetcuionut07@gmail.com>
2025-11-01 12:32:50 +0000

    sum 100

---
+++ Week3_Loops/Seminar/sum_first_100.py
@@ -0,0 +1,9 @@
+
+print("Calculating the sum of the first 100 natural numbers...")
+sum(range(100))
+total = 0
+number = 1
+while number <= 100:
+    total += number
+    number += 1
+print("...Done the answer is", total)
