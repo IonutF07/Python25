9372f84e5c1af277428b36f9a107faff851cde75
Ionut F <fetcuionut07@gmail.com>
2025-11-01 13:02:00 +0000

    loop

---
+++ Week3_Loops/Seminar/for_loop.py
@@ -0,0 +1,11 @@
+count = int(input("How many mountains should I display?\n"))
+
+print("\n Displaying...\n")
+for a in range(count):
+    print("             --")
+    print("            /  \\_")
+    print("           /^    \\")
+    print("          /  ^    \\_")
+    print("        _/  ^^     \\")
+    print("       /            \\")
+print("\n...Done")
