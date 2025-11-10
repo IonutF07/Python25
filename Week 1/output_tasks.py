385ed77a04aa9d99996b5a7ea4046808415385d2
Ionut F <fetcuionut07@gmail.com>
2025-11-10 21:40:27 +0000

    Display messages intro

---
+++ Week 1/output_tasks.py
@@ -0,0 +1,24 @@
+
+# Displaying message to the screen:
+print("Welcome to QH0444!")
+print("In week 1 we will learn...")
+print("....How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+print("I hope you are enjoying the lesson this far")
+
+#Displaying escape characters:
+
+print("\n Displays a new line")
+print("\t Displays a new tab")
+print("\\ Displays a back slash")
+print("\' Displays a single quote")
+
+
+#Display a box
+
+print("##########")
+print("#        #")
+print("#        #")
+print("#        #")
+print("##########")
