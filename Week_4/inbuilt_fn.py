3c050e94687854b2c24ba462fb1c996f7c947d28
Ionut F <fetcuionut07@gmail.com>
2025-11-10 22:28:52 +0000

    Max/Min/Sum/Len - given numers

---
+++ Week_4/inbuilt_fn.py
@@ -0,0 +1,7 @@
+numbers = [5, 10, 15, 20, 25, 40]
+#use built in functions
+print("Enter numbers:",numbers)
+print("Total count= ", len(numbers))
+print("Maximum value", max(numbers))
+print("Minimum value", min(numbers))
+print("Sum of values", sum(numbers))
