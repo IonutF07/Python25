740df86948d0dcd44ba065aa48a4c34417e4d236
Ionut F <fetcuionut07@gmail.com>
2025-11-10 21:40:57 +0000

    Input + display message (robot)

---
+++ Week 1/input_task.py
@@ -0,0 +1,10 @@
+# Ask user for their name printing a robot name
+
+print("What is your name(Initial only)?")
+name = input()
+print(f"It is nice to meet you {name}")
+
+print ("########")
+print (f" {name}    {name} ")
+print ("    -   ")
+print ("########")
