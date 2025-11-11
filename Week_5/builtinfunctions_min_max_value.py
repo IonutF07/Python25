3e06d0f8a89a865968e0c9118a523e973d2150f1
Ionut F <fetcuionut07@gmail.com>
2025-11-11 19:13:52 +0000

    Example of min/max functions using tuple

---
+++ Week_5/builtinfunctions_min_max_value.py
@@ -0,0 +1,6 @@
+# Example of using min() and max() functions with a tuple
+tup2 = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+minval2= min(tup2)
+print("minval2=",minval2)
+maxval= max(tup2)
+print("maxval=",maxval)
