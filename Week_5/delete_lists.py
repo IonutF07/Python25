6df8c125e1ead69b1cc645f9c4ef3bb49f455d98
Ionut F <fetcuionut07@gmail.com>
2025-11-11 19:14:17 +0000

    Deleting elements from list

---
+++ Week_5/delete_lists.py
@@ -0,0 +1,6 @@
+# Deleting elements from a list using the del statement
+list1 = ['physics', 'chemistry', 1997, 2000];
+print (list1)
+del list1[2];
+print ("After deleting value at index 2: ")
+print (list1)
