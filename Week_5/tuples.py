e6cfca5f28ba29aaecab22f32f11338bf0605ff7
Ionut F <fetcuionut07@gmail.com>
2025-11-11 19:13:30 +0000

    Tuples example

---
+++ Week_5/tuples.py
@@ -0,0 +1,7 @@
+#Tuple is a collection which is ordered and unchangeable.
+# In Python tuples are written with round brackets.
+tup1 =("Rohan", "Physics", 21, 69.75)
+tup2 = (1, 2, 3, 4, 5)
+tup3 = ("a", "b", "c", "d")
+tup4 = (25.50, True, -55, 1+2j)
+print(f"tup1:", tup1)
