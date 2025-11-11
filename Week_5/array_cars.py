063823fc66d644b6b4ffc3156997af71bc12a2f9
Ionut F <fetcuionut07@gmail.com>
2025-11-11 19:12:31 +0000

    Car Array + for implication

---
+++ Week_5/array_cars.py
@@ -0,0 +1,9 @@
+#Arrays in Python are implemented using lists.
+# A list is a collection which is ordered and changeable.
+cars = ["Ford", "Volvo", "BMW"]
+print(cars) #this will print entire list as single object
+
+#Example 2
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars: # this will print all car names one by one
+    print(x)
