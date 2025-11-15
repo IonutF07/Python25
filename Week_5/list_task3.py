51c57e0aee932442cad23f7ba25776fc55dee725
Ionut F <fetcuionut07@gmail.com>
2025-11-15 12:46:14 +0000

    Iterate build a menu from a list

---
+++ Week_5/list_task3.py
@@ -0,0 +1,18 @@
+#Simple list - Store Maze Directions
+def directions(): #Define a function called directions.
+    #create a list of maze directions
+    steps = ["Move forward","Move backward","Turn Left","Turn Right"]
+    return steps #return complete list of steps
+def menu():#
+    #Display menu options
+    print("Please select a direction:")
+    steps = directions() # Get the list of directions by calling the directions function.
+    for index in range (len(steps)):
+        direction = steps[index]
+        print(f"{index}:{direction}") 
+
+def run_task3():
+    menu() # Display the menu
+#Ensure task3 runs when this script is executed
+if __name__ == "__main__":
+    run_task3()
