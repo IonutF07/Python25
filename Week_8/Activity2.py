74e8841688503476580f82018b513ae55786cb1c
IonutF07 <fetcuionut07@gmail.com>
2025-12-02 19:27:58 +0000

    Dictionary creation - keys & values

---
+++ Week_8/Activity2.py
@@ -0,0 +1,15 @@
+# Create an empty dictionary and add key-value pairs to it
+wedding = {}
+wedding["Cake"] = 1
+wedding["Car"] = 1
+wedding["Ribbons"] = 100
+print(wedding)
+
+# Iterate through all key-value pairs in the dictionary
+wedding = {"Cakle": 1, "Car": 1, "Ribbons": 100}
+for key in wedding.keys():
+    print(key)
+
+# Iterate through all values in the dictionary
+for value in wedding.values():
+    print(value)
