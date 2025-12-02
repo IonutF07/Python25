5d6685a5ea83fbdf227507f4ca8462bc09dd3f33
IonutF07 <fetcuionut07@gmail.com>
2025-12-02 19:28:15 +0000

    Dictionary of dictionaries.

---
+++ Week_8/Activity3.py
@@ -0,0 +1,9 @@
+#Dictionary of Dictionaries
+wedding = {
+    "Items":{"Cake": 1, "Car": 1, "Ribbons": 100},
+    "People":{"Guests": 30}
+}
+print(wedding)
+# Accessing values in a nested dictionary
+print(wedding["Items"])  # Output: 1
+print(wedding["People"])  # Output: 30
