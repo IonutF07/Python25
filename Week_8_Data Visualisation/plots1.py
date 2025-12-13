68ba9bcf62884a904f68309966af4bc17478a76c
Ionut F <fetcuionut07@gmail.com>
2025-12-13 12:06:28 +0000

    Simple line plot - additional data (x2 lines)

--- Week_8_Data Visualisation/plots1.py
+++ Week_8_Data Visualisation/plots1.py
@@ -7,8 +7,12 @@ def run_task1():
     #Defining the x & y values (y is square of x)
     x_values = [1,2,3,4,5]
     y_values = [1,4,9,16,25]
+    #Adding a and b values
+    a_values = [1,2,3,4,5]
+    b_values = [1,4,9,16,25]
     #Plot the data
-    plt.plot(y_values,x_values)
+    plt.plot(x_values,y_values)
+    plt.plot(a_values,y_values)
     plt.show()
 
 if __name__ == "__main__":
