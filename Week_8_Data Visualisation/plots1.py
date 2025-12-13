2cd0e7bdc78a7e3b182c5dae9889024c4334b85f
Ionut F <fetcuionut07@gmail.com>
2025-12-13 11:56:24 +0000

    Simple line plot - curved reversed

--- Week_8_Data Visualisation/plots1.py
+++ Week_8_Data Visualisation/plots1.py
@@ -12,7 +12,7 @@ def run_task1():
     x_values = [1,2,3,4,5]
     y_values = [1,4,9,16,25]
     #Plot the data
-    display_line(x_values,y_values)
+    display_line(y_values,x_values)
 
 if __name__ == "__main__":
     run_task1()
