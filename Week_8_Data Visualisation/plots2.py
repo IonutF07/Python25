71b9037ea63af4e6106e680a8b18c50e022f09ab
Ionut F <fetcuionut07@gmail.com>
2025-12-13 12:59:35 +0000

    Changes from squares to 3 diamonds in a start shape

--- Week_8_Data Visualisation/plots2.py
+++ Week_8_Data Visualisation/plots2.py
@@ -4,21 +4,25 @@ import matplotlib.pyplot as plt
 def small():
     # Plotting a small square red dotted line and circle markers.
     # Specific numbers that form a small square
-    x = [3, 3, 4, 4, 3]
-    y = [3, 4, 4, 3, 3]
-    plt.plot(x, y, 'r:o') # 'r:o' means: red colour (r), dotted line (:), circle markers (o)
+    x = [4, 3, 2, 3, 4]
+    y = [4, 3, 3, 4, 4]
+    # 'r:o' means: red colour (r), dotted line (:), circle markers (o)
+    plt.plot(x, y, 'r--s')
+
 
 def medium():
-    # Plotting a medium square green dashed line and square markers.
-    x = [2, 2, 5, 5, 2]
-    y = [2, 5, 5, 2, 2]
-    plt.plot(x, y, 'g--s') # 'g--s' means: green (g), dashed line (--), square markers (s)
+    # Plot a medium square using a green dashed line and square markers.
+    x = [4, 3.5, 4, 4.5, 4]
+    y = [4, 5, 6, 5, 4]
+    # 'g--s' means: green (g), dashed line (--), square markers (s)
+    plt.plot(x, y, 'b-p')
+
 
 def large():
-    # Plotting a large square blue solid line and pentagon markers.
-    x = [1, 1, 6, 6, 1]
-    y = [1, 6, 6, 1, 1]
-    plt.plot(x, y, 'b-p') # 'b-p' means: blue (b), solid line (-), pentagon markers (p)
+    # Plot a large square using a blue solid line and pentagon markers.
+    x = [4, 5, 6, 5, 4]
+    y = [4, 4, 3, 3, 4]
+    plt.plot(x, y, 'g--p') # 'b-p' means: blue (b), solid line (-), pentagon markers (p)
 
 def run_task2():
     small()
