2c052ea610ffc86d993ef485cad5261d32ddbb0c
Ionut F <fetcuionut07@gmail.com>
2025-12-13 11:59:14 +0000

    Simple line plot - simplifying the code by removing def x/y function

--- Week_8_Data Visualisation/plots1.py
+++ Week_8_Data Visualisation/plots1.py
@@ -1,10 +1,6 @@
 #Simple Line Plot
 import matplotlib.pyplot as plt
 
-def display_line(x,y):#Display the plot for x and y
-    plt.plot(x,y) #Drawing a basic line plot
-    plt.show() #Render the plot window
-
 def run_task1():
     #Task1 - creating a sample x and y data plot
     print("Running...")
@@ -12,7 +8,8 @@ def run_task1():
     x_values = [1,2,3,4,5]
     y_values = [1,4,9,16,25]
     #Plot the data
-    display_line(y_values,x_values)
+    plt.plot(y_values,x_values)
+    plt.show()
 
 if __name__ == "__main__":
     run_task1()
