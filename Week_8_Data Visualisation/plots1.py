9f391d53859016ce84d2605a7d63e00d7154c204
Ionut F <fetcuionut07@gmail.com>
2025-12-13 11:55:58 +0000

    Simple line plot

---
+++ Week_8_Data Visualisation/plots1.py
@@ -0,0 +1,18 @@
+#Simple Line Plot
+import matplotlib.pyplot as plt
+
+def display_line(x,y):#Display the plot for x and y
+    plt.plot(x,y) #Drawing a basic line plot
+    plt.show() #Render the plot window
+
+def run_task1():
+    #Task1 - creating a sample x and y data plot
+    print("Running...")
+    #Defining the x & y values (y is square of x)
+    x_values = [1,2,3,4,5]
+    y_values = [1,4,9,16,25]
+    #Plot the data
+    display_line(x_values,y_values)
+
+if __name__ == "__main__":
+    run_task1()
